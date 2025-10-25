6d0e11020f2a4d0785c01e362a1ee22d36ac1572
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-10-25 22:08:37 +0600

    clean API

--- src/root.zig
+++ src/root.zig
@@ -7,7 +7,14 @@ comptime {
 }
 
 const tests = @import("./tests.zig");
-pub const serde = @import("./serde.zig");
+const serde = @import("./serde.zig");
+
+pub const serialize = serde.serialize;
+pub const SerializeError = serde.SerializeError;
+
+pub const deserialize = serde.deserialize;
+pub const deserialize_stream = serde.deserialize_stream;
+pub const DeserializeError = serde.DeserializeError;
 
 test {
     _ = tests;
